4eb5dd281ec968fe1c57058d786969684a4b3376
opalopal09 <opalopal09@gmail.com>
2026-04-10 01:57:11 -0700

    Fix: display validation error messages on kendaraan create/edit forms

--- resources/views/kendaraan/create.blade.php
+++ resources/views/kendaraan/create.blade.php
@@ -7,10 +7,20 @@
 <form method="POST" action="/kendaraan">
 @csrf
 
-<input name="plat_nomor" class="form-control mb-2" placeholder="Plat Nomor">
-<input name="jenis" class="form-control mb-2" placeholder="Jenis Kendaraan">
-<input name="warna" class="form-control mb-2" placeholder="Warna">
-<input name="pemilik" class="form-control mb-2" placeholder="Pemilik">
+@if ($errors->any())
+    <div class="alert alert-danger mb-3">
+        <ul class="mb-0">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<input name="plat_nomor" class="form-control mb-2" placeholder="Plat Nomor" value="{{ old('plat_nomor') }}">
+<input name="jenis" class="form-control mb-2" placeholder="Jenis Kendaraan" value="{{ old('jenis') }}">
+<input name="warna" class="form-control mb-2" placeholder="Warna" value="{{ old('warna') }}">
+<input name="pemilik" class="form-control mb-2" placeholder="Pemilik" value="{{ old('pemilik') }}">
 
 <button class="btn btn-success">Simpan</button>
 <a href="/kendaraan" class="btn btn-secondary">Kembali</a>
--- resources/views/kendaraan/edit.blade.php
+++ resources/views/kendaraan/edit.blade.php
@@ -8,10 +8,20 @@
 @csrf
 @method('PUT')
 
-<input name="plat_nomor" value="{{ $kendaraan->plat_nomor }}" class="form-control mb-2" placeholder="Plat Nomor">
-<input name="jenis" value="{{ $kendaraan->jenis }}" class="form-control mb-2" placeholder="Jenis Kendaraan">
-<input name="warna" value="{{ $kendaraan->warna }}" class="form-control mb-2" placeholder="Warna">
-<input name="pemilik" value="{{ $kendaraan->pemilik }}" class="form-control mb-2" placeholder="Pemilik">
+@if ($errors->any())
+    <div class="alert alert-danger mb-3">
+        <ul class="mb-0">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<input name="plat_nomor" value="{{ old('plat_nomor', $kendaraan->plat_nomor) }}" class="form-control mb-2" placeholder="Plat Nomor">
+<input name="jenis" value="{{ old('jenis', $kendaraan->jenis) }}" class="form-control mb-2" placeholder="Jenis Kendaraan">
+<input name="warna" value="{{ old('warna', $kendaraan->warna) }}" class="form-control mb-2" placeholder="Warna">
+<input name="pemilik" value="{{ old('pemilik', $kendaraan->pemilik) }}" class="form-control mb-2" placeholder="Pemilik">
 
 <button class="btn btn-primary">Update</button>
 <a href="/kendaraan" class="btn btn-secondary">Kembali</a>
